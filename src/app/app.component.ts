a9bbb16134853aa6a723491d9af2b7cc821f52e7
heryrmdn <heryrhamadan@gmail.com>
2024-06-12 15:01:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './core/Item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-to-do-list';
+  desc: any;
+  data: Item = {
+    item: []
+  };
+
+  constructor() {
+    this.getTask();
+  }
+
+  getTask() {
+    let dataLocal = localStorage.getItem("localStorage");
+    if (dataLocal !== null) {
+      this.data = JSON.parse(dataLocal);
+    } else {
+      this.data['item'] = [];
+    }
+  }
+
+  saveTask() {
+    this.data['item'].unshift({
+      id: Math.floor((Math.random() * 1000) + 1),
+      description: this.desc,
+      status: false
+    });
+    localStorage.clear();
+    localStorage.setItem("localStorage", JSON.stringify({ item: this.data['item'] }))
+    this.reset();
+  }
+
+  doneTask(id: number) {
+    for (const i in this.data['item']) {
+      if (this.data['item'][i].id == id) {
+        this.data['item'][i].status = true;
+        break;
+      }
+    }
+    localStorage.clear();
+    localStorage.setItem("localStorage", JSON.stringify({ item: this.data['item'] }))
+  }
+
+  deleteTask(id: number) {
+    this.data.item = this.data.item.filter(v => v.id !== id);
+    localStorage.clear();
+    localStorage.setItem("localStorage", JSON.stringify({ item: this.data['item'] }))
+  }
+
+  reset() {
+    this.desc = '';
+    this.getTask();
+  }
+
 }
